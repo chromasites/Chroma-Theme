c5013fe011983f6762a416e1799c4317ca115b32
Nate Kruska <nskmail@gmail.com>
2016-02-14 23:24:26 -0500

    CDN Font Awesome

--- inc/functions/assets.php
+++ inc/functions/assets.php
@@ -9,6 +9,17 @@
 // Enqueue styles and scripts.
 function chroma_scripts() {
 	wp_enqueue_style( 'chroma-style', get_stylesheet_uri() );
+	wp_enqueue_style(
+		'font-awesome.css',
+		'https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css',
+		array(),
+		'4.5.0'
+	);
+	wp_enqueue_style(
+		'Google Fonts',
+		'https://fonts.googleapis.com/',
+		array()
+	);
 	wp_enqueue_script(
 		'jquery',
 		true
