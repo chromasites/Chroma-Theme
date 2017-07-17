787f7630efaa824ec9531ead209d50e11f8b1e8d
Nate Kruska <nskmail@gmail.com>
2017-07-17 10:41:51 -0400

    New archive display options
    Add archive displays to theme customizer
    Options for single column layouts and featured images
    New post meta template tags
    Remove single post layout options

---
+++ template-parts/post-archive-meta.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Template part for displaying meta information for posts.
+ *
+ * @package Chroma_Theme
+ */
+?>
+
+<div class="entry-meta">
+
+<?php
+
+chroma_category_links();
+
+chroma_posted_by();
+
+chroma_posted_on();
+
+chroma_comment_link();
+
+?>
+
+</div>
---
+++ template-parts/post-single-footer.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Template part for displaying post footer information
+ *
+ * @package Chroma_Theme
+ */
+
+chroma_updated_on();
+
+chroma_category_links();
+
+chroma_tag_links();
